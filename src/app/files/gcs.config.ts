0ea5f88500d1784d1fda39413b6b08b61bec9753
Meyrambek Kadyrbekov <mkadyrbekov@divtech.kz>
2023-05-26 19:53:58 +0600

    fix files

---
+++ src/app/files/gcs.config.ts
@@ -0,0 +1,7 @@
+import { StorageOptions } from '@google-cloud/storage';
+import * as path from 'path';
+
+export const GCS_CONFIG: StorageOptions = {
+  projectId: process.env.PROJECT_KEY,
+  keyFilename: path.join(__dirname, `../../../key.json`),
+};
